1eeba5d8045005b1c760c6947289205bb643db0e
Glebson Santos <glebsonsantos@outlook.pt>
2024-11-24 23:00:44 -0300

    feat: Add UnitOfWork

---
+++ Desafio.BackEnd/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace Desafio.BackEnd.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepositoryEvent RepositoryEvent { get; }
+        IRepositoryPanelist RepositoryPanelist { get; }
+
+        Task CommitAsync();
+    }
+}
---
+++ Desafio.BackEnd/Repositories/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using Desafio.BackEnd.Context;
+using Desafio.BackEnd.Interfaces;
+
+namespace Desafio.BackEnd.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _context;
+
+        private IRepositoryEvent? repositoryEvent;
+        private IRepositoryPanelist? repositoryPanelist;
+
+        public UnitOfWork(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IRepositoryEvent RepositoryEvent { 
+            get 
+            { 
+                return repositoryEvent = repositoryEvent ?? new RepositoryEvent(_context);
+            } 
+        }
+
+        public IRepositoryPanelist RepositoryPanelist
+        {
+            get
+            {
+                return repositoryPanelist = repositoryPanelist ?? new RepositoryPanelist(_context);
+            }
+
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
