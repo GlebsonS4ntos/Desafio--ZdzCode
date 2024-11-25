505269d17ec530694cb7c2a3ecf549cef709835b
Glebson Santos <glebsonsantos@outlook.pt>
2024-11-24 21:45:27 -0300

    feat: Add Entities Classes

---
+++ Desafio.BackEnd/Entities/Event.cs
@@ -0,0 +1,12 @@
+﻿namespace Desafio.BackEnd.Entities
+{
+    public class Event
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Place {  get; set; }
+        public long PanelistId { get; set; }
+        public Panelist Panelist { get; set; }
+    }
+}
---
+++ Desafio.BackEnd/Entities/Panelist.cs
@@ -0,0 +1,11 @@
+﻿namespace Desafio.BackEnd.Entities
+{
+    public class Panelist
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Job {  get; set; }   
+        public List<Event> Events { get; set; } 
+    }
+}
