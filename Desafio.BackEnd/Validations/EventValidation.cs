74317536b45ad1febbcce388d254f59d0d23e8da
Glebson Santos <glebsonsantos@outlook.pt>
2024-11-25 21:36:18 -0300

    feat: Add Validations using Fluent Validation

---
+++ Desafio.BackEnd/Validations/EventValidation.cs
@@ -0,0 +1,39 @@
+﻿using Desafio.BackEnd.Dtos;
+using FluentValidation;
+
+namespace Desafio.BackEnd.Validations
+{
+    public class EventValidation : AbstractValidator<EventDto>
+    {
+        public EventValidation() 
+        {
+            RuleFor(e => e.Date)
+                .NotNull().WithMessage("A data do evento é obrigatória.")
+                .NotEmpty().WithMessage("A data do evento é obrigatória.")
+                .Custom((date, context) =>
+                {
+                    var dateNow = DateTime.Now;
+                    if (date < dateNow)
+                    {
+                        context.AddFailure("A data do evento deve ser maior que a data Atual");
+                    }
+                });
+
+            RuleFor(e => e.Name)
+                .NotEmpty().WithMessage("O campo Nome é obrigatório")
+                .NotNull().WithMessage("O campo Nome é obrigatório")
+                .MinimumLength(3).WithMessage("O campo Nome deve possuir no minimo 3 Caracteres.")
+                .MaximumLength(100).WithMessage("O campo Nome dever possuir no maximo 100 Caracteres.");
+
+            RuleFor(e => e.Place)
+                .NotEmpty().WithMessage("O campo Lugar é obrigatório")
+                .NotNull().WithMessage("O campo Lugar é obrigatório")
+                .MinimumLength(3).WithMessage("O campo Lugar deve possuir no minimo 5 Caracteres.")
+                .MaximumLength(100).WithMessage("O campo Lugar dever possuir no maximo 50 Caracteres.");
+
+            RuleFor(e => e.PanelistId)
+                .NotEmpty().WithMessage("O campo Palestrante é obrigatório")
+                .NotNull().WithMessage("O campo Palestrante é obrigatório");
+        }
+    }
+}
---
+++ Desafio.BackEnd/Validations/PanelistValidation.cs
@@ -0,0 +1,35 @@
+﻿using Desafio.BackEnd.Dtos;
+using FluentValidation;
+
+namespace Desafio.BackEnd.Validations
+{
+    public class PanelistValidation : AbstractValidator<PanelistDto>
+    {
+        public PanelistValidation() 
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("O campo Nome é obrigatório")
+                .NotNull().WithMessage("O campo Nome é obrigatório")
+                .MinimumLength(3).WithMessage("O campo Nome deve possuir no minimo 3 Caracteres.")
+                .MaximumLength(100).WithMessage("O campo Nome dever possuir no maximo 100 Caracteres.");
+
+            RuleFor(p => p.BirthDate)
+                .NotNull().WithMessage("O campo Data de Aniversário é obrigatorio.")
+                .NotEmpty().WithMessage("O campo Data de Aniversário é obrigatorio.")
+                .Custom((birthDate, context) =>
+                {
+                    var minBirthDate = DateTime.Now.AddYears(-18);
+                    if (birthDate <= minBirthDate)
+                    {
+                        context.AddFailure("O Palestrante deve possuir mais de 18 anos.");
+                    }
+                });
+
+            RuleFor(p => p.Job)
+                .NotNull().WithMessage("O campo Trabalho é obrigatorio.")
+                .NotEmpty().WithMessage("O campo Trabalho é obrigatorio.")
+                .MinimumLength(3).WithMessage("O campo Trabalho deve possuir no minimo 3 Caracteres.")
+                .MaximumLength(70).WithMessage("O campo Trabalho dever possuir no maximo 70 Caracteres.");
+        }
+    }
+}
