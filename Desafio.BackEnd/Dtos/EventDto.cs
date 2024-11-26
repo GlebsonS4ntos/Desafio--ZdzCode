bf23d4c12a88b333af56a120bb7465bf32a9934c
Glebson Santos <glebsonsantos@outlook.pt>
2024-11-25 21:36:48 -0300

    feat: Add Dtos

---
+++ Desafio.BackEnd/Dtos/EventDto.cs
@@ -0,0 +1,14 @@
+﻿using Desafio.BackEnd.Entities;
+
+namespace Desafio.BackEnd.Dtos
+{
+    public class EventDto
+    {
+        public long? Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? Date { get; set; }
+        public string? Place { get; set; }
+        public long? PanelistId { get; set; }
+        public Panelist? Panelist { get; set; }
+    }
+}
---
+++ Desafio.BackEnd/Dtos/PanelistDto.cs
@@ -0,0 +1,13 @@
+﻿using Desafio.BackEnd.Entities;
+
+namespace Desafio.BackEnd.Dtos
+{
+    public class PanelistDto
+    {
+        public long? Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? Job { get; set; }
+        public List<Event>? Events { get; set; }
+    }
+}
