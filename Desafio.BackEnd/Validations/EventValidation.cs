1fde77b56aad8ab49cb2660f6543b2c35c7d5cd1
Glebson Santos <glebsonsantos@outlook.pt>
2024-11-27 09:26:43 -0300

    fix: Fix Validation

--- Desafio.BackEnd/Validations/EventValidation.cs
+++ Desafio.BackEnd/Validations/EventValidation.cs
@@ -8,32 +8,33 @@ namespace Desafio.BackEnd.Validations
         public EventValidation() 
         {
             RuleFor(e => e.Date)
-                .NotNull().WithMessage("A data do evento é obrigatória.")
-                .NotEmpty().WithMessage("A data do evento é obrigatória.")
+                .NotNull().NotEmpty().WithMessage("A data do evento é obrigatória.")
+                .Must(birthDate => birthDate != null && birthDate.Value.Kind == DateTimeKind.Utc).WithMessage("A data deve estar em formato válido e ser UTC.")
                 .Custom((date, context) =>
                 {
-                    var dateNow = DateTime.Now;
-                    if (date < dateNow)
+                    if (date.HasValue) // Verifica se o valor não é nulo
                     {
-                        context.AddFailure("A data do evento deve ser maior que a data Atual");
+                        var dateNow = DateTime.UtcNow;
+                        if (date.Value <= dateNow)
+                        {
+                            context.AddFailure("A data do evento deve ser maior que a data atual.");
+                        }
                     }
                 });
+        
 
             RuleFor(e => e.Name)
-                .NotEmpty().WithMessage("O campo Nome é obrigatório")
-                .NotNull().WithMessage("O campo Nome é obrigatório")
+                .NotEmpty().NotNull().WithMessage("O campo Nome é obrigatório")
                 .MinimumLength(3).WithMessage("O campo Nome deve possuir no minimo 3 Caracteres.")
                 .MaximumLength(100).WithMessage("O campo Nome dever possuir no maximo 100 Caracteres.");
 
             RuleFor(e => e.Place)
-                .NotEmpty().WithMessage("O campo Lugar é obrigatório")
-                .NotNull().WithMessage("O campo Lugar é obrigatório")
+                .NotEmpty().NotNull().WithMessage("O campo Lugar é obrigatório")
                 .MinimumLength(3).WithMessage("O campo Lugar deve possuir no minimo 5 Caracteres.")
                 .MaximumLength(100).WithMessage("O campo Lugar dever possuir no maximo 50 Caracteres.");
 
             RuleFor(e => e.PanelistId)
-                .NotEmpty().WithMessage("O campo Palestrante é obrigatório")
-                .NotNull().WithMessage("O campo Palestrante é obrigatório");
+                .NotEmpty().NotNull().WithMessage("O campo Palestrante é obrigatório");
         }
     }
 }
--- Desafio.BackEnd/Validations/PanelistValidation.cs
+++ Desafio.BackEnd/Validations/PanelistValidation.cs
@@ -8,26 +8,27 @@ namespace Desafio.BackEnd.Validations
         public PanelistValidation() 
         {
             RuleFor(p => p.Name)
-                .NotEmpty().WithMessage("O campo Nome é obrigatório")
-                .NotNull().WithMessage("O campo Nome é obrigatório")
+                .NotEmpty().NotNull().WithMessage("O campo Nome é obrigatório")
                 .MinimumLength(3).WithMessage("O campo Nome deve possuir no minimo 3 Caracteres.")
                 .MaximumLength(100).WithMessage("O campo Nome dever possuir no maximo 100 Caracteres.");
 
             RuleFor(p => p.BirthDate)
-                .NotNull().WithMessage("O campo Data de Aniversário é obrigatorio.")
-                .NotEmpty().WithMessage("O campo Data de Aniversário é obrigatorio.")
+                .NotNull().NotEmpty().WithMessage("O campo Data de Aniversário é obrigatorio.")
+                .Must(birthDate => birthDate != null && birthDate.Value.Kind == DateTimeKind.Utc).WithMessage("A data deve estar em formato válido e ser UTC.")
                 .Custom((birthDate, context) =>
                 {
-                    var minBirthDate = DateTime.Now.AddYears(-18);
-                    if (birthDate <= minBirthDate)
+                    if (birthDate.HasValue)
                     {
-                        context.AddFailure("O Palestrante deve possuir mais de 18 anos.");
+                        var minBirthDate = DateTime.Now.AddYears(-18);
+                        if (birthDate.Value > minBirthDate)
+                        {
+                            context.AddFailure("O Palestrante deve possuir mais de 18 anos.");
+                        }
                     }
                 });
 
             RuleFor(p => p.Job)
-                .NotNull().WithMessage("O campo Trabalho é obrigatorio.")
-                .NotEmpty().WithMessage("O campo Trabalho é obrigatorio.")
+                .NotNull().NotEmpty().WithMessage("O campo Trabalho é obrigatorio.")
                 .MinimumLength(3).WithMessage("O campo Trabalho deve possuir no minimo 3 Caracteres.")
                 .MaximumLength(70).WithMessage("O campo Trabalho dever possuir no maximo 70 Caracteres.");
         }
